31b1692961b167406fdfd94ab1fec8b2c89a0207
Filip Rac <101470966+filipracGITHUB@users.noreply.github.com>
2022-03-30 16:57:03 +0200

    Update Post.js

--- src/Post.js
+++ src/Post.js
@@ -11,7 +11,7 @@ const Post = forwardRef(({ name, description, message, photoUrl }, ref) => {
   return (
     <div ref={ref} className="post">
       <div className="post__header">
-        <Avatar src={photoUrl}>{name[0].toUpperCase()}</Avatar>
+        <Avatar src={photoUrl}></Avatar>
         <div className="post__info">
           <h2>{name}</h2>
           <p>{description}</p>
